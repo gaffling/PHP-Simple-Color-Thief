ebe9d213bc4176a61dda20b530666a7e41398e5a
I. Gaffling <10500034+gaffling@users.noreply.github.com>
2021-08-05 10:41:21 +0200

    Update color.php

--- color.php
+++ color.php
@@ -10,7 +10,7 @@
  */
 
 function simple_color_thief($image, $default_color='eee') {
-  if ($original_image = imagecreatefromstring(@file_get_contents($image))) {
+  if ($original_image = @imagecreatefromstring(@file_get_contents($image))) {
     $shortend_image = imagecreatetruecolor(1, 1);
     imagecopyresampled($shortend_image, $original_image, 0, 0, 0, 0, 1, 1, imagesx($original_image), imagesy($original_image));
     return dechex(imagecolorat($shortend_image, 0, 0));
